1dfe9c1c3d4de71fecb2b76cd7fe620516f3bb12
Anouck Celis <anouck.celis@gmail.com>
2024-04-30 17:21:54 +0200

    logboek pagina toevoegen button

--- src/logbook.js
+++ src/logbook.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './logbook.css';
+import { FaArrowLeft } from 'react-icons/fa';
+
 
 const Logbook = () => {
-   
+  
+  const handleBackToLastPage = () => {
+    window.history.back();
+  };
+
   return (
     <div className="logbook-container">
       <h1>Logboek</h1>
-    </div>
       
+      <div className="back-button-container">
+        <button className="back-button" onClick={handleBackToLastPage}><FaArrowLeft /></button>
+      </div>
+    </div>
   );
-};
-
+}
 
 export default Logbook;
